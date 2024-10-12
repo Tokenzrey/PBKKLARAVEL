425eef5749bbc91aef83709025329de79c0172f5
RafliPane15 <discordtupoino@gmail.com>
2024-10-13 01:11:06 +0700

    first commit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -12,7 +12,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,7 +20,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
@@ -32,7 +32,7 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password_confirmation" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
@@ -44,7 +44,7 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="ms-4 bg-[#00B4D8]">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
