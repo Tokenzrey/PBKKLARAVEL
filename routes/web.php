57de2b4d43557cd9b163f8fa216c88cf02251d6e
LittleRedRidingHood73 <fauzandzikri73@gmail.com>
2024-10-13 00:02:52 +0700

    menambahkan controller peminjaman pada route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CekSesi;
+use App\Http\Controllers\PeminjamanController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,5 +39,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/peminjaman', [PeminjamanController::class, 'index'])->name('peminjaman.index');
 
 require __DIR__.'/auth.php';
