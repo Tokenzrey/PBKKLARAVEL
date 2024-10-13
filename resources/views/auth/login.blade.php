c6e7224bf9abbbe60fe0d213e86408b2e218dff5
RafliPane15 <discordtupoino@gmail.com>
2024-10-13 14:50:06 +0700

    Fix Collor Login Register Page and Update data

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,9 @@
-<x-guest-layout>
-            <h2 class="text-3xl font-bold text-center mb-6 text-gray-700">Login to Your Account</h2>
+    <x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <!-- Session Status -->
-            <x-auth-session-status class="mb-4" :status="session('status')" />
-
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
                 <!-- Email Address -->
                 <div class="relative mb-4">
@@ -32,12 +30,12 @@
             </label>
         </div>
 
-                <div class="flex items-center justify-between mt-4">
-                    @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                    @endif
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
 
             <x-primary-button class="ms-3" style="background-color: #00B4D8;">
                 {{ __('Log in') }}
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -8,14 +8,14 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B3D6] focus:border-[#00B3D6]" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B4D8] focus:border-[#00B4D8]" type="password" name="password" required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full p-2 pl-4 border border-gray-300 rounded-lg focus:ring-1 focus:ring-[#00B3D6] focus:border-[#00B3D6]" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
